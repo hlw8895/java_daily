c1af8f4238ee23fbb4cf358cd721e084f39b258d
hlw8895 <60841139+hlw8895@users.noreply.github.com>
2020-10-15 00:46:57 +0800

    preorderTraversal

--- test/demo/src/binary_tree.java
+++ test/demo/src/binary_tree.java
@@ -24,6 +24,18 @@ public class binary_tree {
     }
 
     class qianxv {
-        
+        private List<Integer> list = new ArrayList<>();
+        public List<Integer> preorderTraversal(TreeNode root) {
+            preorder(root);
+            return list;
+        }
+        public void preorder (TreeNode root){
+            if (root == null)
+                return;
+            list.add(root.val);
+            preorder(root.left);
+            preorder(root.right);
+        }
+       
     }
 }
